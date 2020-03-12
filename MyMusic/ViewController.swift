7ec3ae06d6ab086b82004200db8bcabef1377927
I.D <daisuke.workin@gmail.com>
2020-03-12 10:09:27 +0900

    initialize

---
+++ MyMusic/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyMusic
+//
+//  Created by 石澤大輔 on 2020/03/12.
+//  Copyright © 2020 石澤大輔. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
